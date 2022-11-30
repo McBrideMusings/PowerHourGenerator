a973df6601f72bef31c039c38be8259072cec437
Pierce McBride <piercermcbride@gmail.com>
2022-11-30 10:12:26 -0500

    Added argparse to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 import list_processor
 import video_processor
@@ -14,14 +15,20 @@ test_padding_x = 100
 test_padding_y = 100
 
 
-def parse_args(argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--x', type=int, required=True)
-    parser.add_argument('--y', type=int, required=True)
-    args = parser.parse_args()
+def main_args():
+    parser = argparse.ArgumentParser(description=main.__doc__)
+    args = parse_args()
+    if not args.input or os.path.exists(args.input):
+        parser.print_help()
+        sys.exit(1)
+    else:
+        project = args.project
+        if project is None:
+            project = str(uuid.uuid4())
+        main(args.input, args.project, args.resolution, args.limit)
 
 
-def main(input_path: str, project_name: str, target_res: str = "1080p", song_limit: int = 0):
+def main(input_path: str, project_name: str, target_res: str = "1080p", song_limit: int = 0, start: int = 1):
     main_songs = list_processor.parse_list(input_path)
     main_config = PowerHourConfig(project_name)
     # Concat doesn't work right now, no need to process the interstitial
@@ -32,15 +39,59 @@ def main(input_path: str, project_name: str, target_res: str = "1080p", song_lim
         main_songs = main_songs[:song_limit]
     print(f"song length {len(main_songs)}")
     for song in main_songs:
-        song_path = video_processor.download_song(main_config, song)
-        scale, letterbox = video_processor.should_scale_letterbox(target_res, song_path)
-        if scale or letterbox:
-            song_path = video_processor.scale_letterbox_video(scale, letterbox, target_res, song_path)
-        song_path = video_processor.process_song_effects(main_config, song, num, song_path)
-        song_path_list.append(song_path)
+        if num >= start:
+            song_path = video_processor.download_song(main_config, song)
+            scale, letterbox = video_processor.should_scale_letterbox(target_res, song_path)
+            if scale or letterbox:
+                song_path = video_processor.scale_letterbox_video(scale, letterbox, target_res, song_path)
+            song_path = video_processor.process_song_effects(main_config, song, num, song_path)
+            path, filename = os.path.split(song_path)
+            filename = os.path.splitext(filename)[0]
+            newfilename = f"{project_name}.{num:02d}.{song.get_filename('mp4')}"
+            newpath = os.path.join(path, newfilename)
+            os.rename(song_path, newpath)
+            song_path_list.append(song_path)
         num = num + 1
     # Concat doesn't work correctly right now
     # video_processor.concat_power_hour(main_config, interstitial_processed, song_path_list)
+
+
+def parse_args(parser: argparse.ArgumentParser) -> argparse.Namespace:
+    parser.add_argument(
+        "-i",
+        "--input",
+        help=(
+            "Path to a formatted csv or tsv"
+            "Required, enter -example to generate an example file"
+        ),
+    )
+    parser.add_argument(
+        "-p",
+        "--project",
+        help=(
+            "Name of the video project"
+            "Will generate a folder with this name and will be used in file names"
+        ),
+    )
+    parser.add_argument(
+        "-r",
+        "--resolution",
+        const="mp4",
+        help=(
+            "Resolution of the project, eg 1080p, 720p"
+            "All videos will be scaled to this resolution, letterboxed if necessary"
+        ),
+    )
+    parser.add_argument(
+        "-l",
+        "--limit",
+        const="0",
+        help=(
+            "Number of songs to download and process, starting from the first"
+            "Defaults to downloading the entire input"
+        ),
+    )
+    return parser.parse_args()
 
 
 # Put IDE Debug stuff here
@@ -71,4 +122,3 @@ if __name__ == "__main__":
         if os.getenv("SONG_LIMIT") is not None:
             env_song_limit = int(os.getenv("SONG_LIMIT"))
         main(test_import_tsv, str(uuid.uuid4()), target_res="1080p", song_limit=env_song_limit)
-
